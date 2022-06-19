e4b194fde7af91d5129b707b980d935d3fca058d
Ilia Loginov <iloginow@Ilias-MacBook-Pro.local>
2022-06-19 19:19:41 +0300

    feat: validator class. requirement class

---
+++ index.d.ts
@@ -0,0 +1,36 @@
+/// <reference types="node" />
+
+declare module 'vurts-validator' {
+  export enum ErrorMessages {
+    INVALID_REQUIREMENT_LIST,
+    INVALID_REQUIREMENT,
+    INVALID_REQUIREMENT_SUBJECT,
+    INVALID_REQUIREMENT_MESSAGE,
+  };
+
+  export type RequirementSubject = (data: any) => boolean;
+
+  export type RequirementContext = {
+    subject: RequirementSubject,
+    message: string,
+  };
+
+  export class Requirement {
+    subject: RequirementSubject;
+    message: string;
+
+    constructor(ctx: RequirementContext);
+    check(data: any): boolean;
+  }
+
+  export type ValidatorContext = {
+    requirements: Requirement[],
+  };
+
+  export class Validator {
+    requirements: Requirement[];
+
+    constructor(ctx: ValidatorContext);
+    validate(data: any): boolean;
+  }
+}
---
+++ index.js
@@ -0,0 +1,5 @@
+import Validator from './lib/validator.js';
+import Requirement from './lib/requirement.js';
+import errorMessages from './lib/constants/error-messages.js';
+
+export { Validator, Requirement, errorMessages };
